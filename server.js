d30e451955e9ddf55d26f3406cd3cb2e6a89fdae
Faaz <faaz.iqbal@gmail.com>
2017-05-17 15:30:30 +0200

    Message changed

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const port = 8080;
 var onRequest = function(req, res){
   console.log(req.method+': '+req.url);
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('Hello World!');
+  res.write('Hello Team!');
   res.end();
 }
 http.createServer(onRequest).listen(port);
